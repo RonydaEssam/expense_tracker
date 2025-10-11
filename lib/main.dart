d9869b64f29535ebe5c47f9c0f601c2093d8b73c
RonydaEssam <ronyda988@gmail.com>
2025-10-11 17:08:17 +0300

    create the expenses home page stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tracking Expenses',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Tracking Expenses')),
-        backgroundColor: Colors.white,
-      ),
+      home: Expenses(),
     );
   }
 }
